b5874e60968f5dbc1cb384f4dba92128c7dc83c0
softwarespot <softwarespotapps@gmail.com>
2025-02-27 16:48:40 +0200

    Changed to %q

--- internal/logging/logger.go
+++ internal/logging/logger.go
@@ -19,12 +19,12 @@ func NewLogger() *Logger {
 
 func (l *Logger) LogError(err error) {
 	if _, outErr := l.errOut.WriteString(err.Error() + "\n"); outErr != nil {
-		fmt.Fprintf(os.Stderr, "skipped logging entry due to error: %+v. Message: '%s'\n", outErr, err.Error())
+		fmt.Fprintf(os.Stderr, "skipped logging entry due to error: %+v. Message: %q\n", outErr, err.Error())
 	}
 }
 
 func (l *Logger) Log(msg string) {
 	if _, err := l.out.WriteString(msg + "\n"); err != nil {
-		fmt.Fprintf(os.Stderr, "skipped logging entry due to error: %+v. Message: '%s'\n", err, msg)
+		fmt.Fprintf(os.Stderr, "skipped logging entry due to error: %+v. Message: %q\n", err, msg)
 	}
 }
